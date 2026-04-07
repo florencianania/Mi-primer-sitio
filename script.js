494194af35cf4a7868a770138a549d5891c754fa
Florencia Nania <mariaflorencianania@gmail.com>
2026-04-07 15:23:43 -0300

    ordenar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const botonMenu = document.getElementById('mobile-menu');
 const listaEnlaces = document.getElementById('nav-list');
 
-
 botonMenu.addEventListener('click', () => {
     listaEnlaces.classList.toggle('show');
 });
