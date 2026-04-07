f55205261d8175dfda17926cde37e9dc4b889c6b
Florencia Nania <mariaflorencianania@gmail.com>
2026-04-07 14:54:07 -0300

    agrego menu hamburguesa

---
+++ script.js
@@ -0,0 +1,7 @@
+const botonMenu = document.getElementById('mobile-menu');
+const listaEnlaces = document.getElementById('nav-list');
+
+
+botonMenu.addEventListener('click', () => {
+    listaEnlaces.classList.toggle('show');
+});
